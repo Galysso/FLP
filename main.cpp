0a49cc923af8329574bea4414b20c7e087c6fbd1
Galysso <jocelin.cailloux@gmail.com>
2016-11-25 08:20:42 +0100

    plus de temps cpu

--- main.cpp
+++ main.cpp
@@ -9,44 +9,13 @@ using namespace std;
 int main(int argc, char* argv[]) {
 	clock_t debut, fin;
 	
-	Donnees d20x10_1("datas/20x10_1.dat");
-	Donnees d20x10_2("datas/20x10_2.dat");
-	Donnees d20x10_3("datas/20x10_3.dat");
-	Donnees d20x10_4("datas/20x10_4.dat");			// Ici, l'heuristique trouve vite la solution
-	Donnees d30x15_1("datas/30x15_1.dat");
-	Donnees d30x15_2("datas/30x15_2.dat");
-	//~ Donnees d("datas/test.dat");
-	
+	Donnees d("datas/75x30.dat");
 	//~ d.afficherCoutsAllocation();
 	//~ d.afficherDemandes();
 	//~ d.afficherCoutsOuverture();
 	//~ d.afficherCapacites();
 	
-	SSCFLP sscflp(&d20x10_1);
-	sscflp.glpkModeliserProbleme();
-	debut = clock();
-	sscflp.glpkResoudreProbleme();
-	fin = clock();
-	sscflp.glpkAfficherSolution();
-	cout << "Résolution en " << (fin-debut)*1000/CLOCKS_PER_SEC << " millisecondes" << endl << endl;
-	
-	sscflp.setDonnees(&d20x10_2);
-	sscflp.glpkModeliserProbleme();
-	debut = clock();
-	sscflp.glpkResoudreProbleme();
-	fin = clock();
-	sscflp.glpkAfficherSolution();
-	cout << "Résolution en " << (fin-debut)*1000/CLOCKS_PER_SEC << " millisecondes" << endl << endl;
-	
-	sscflp.setDonnees(&d20x10_3);
-	sscflp.glpkModeliserProbleme();
-	debut = clock();
-	sscflp.glpkResoudreProbleme();
-	fin = clock();
-	sscflp.glpkAfficherSolution();
-	cout << "Résolution en " << (fin-debut)*1000/CLOCKS_PER_SEC << " millisecondes" << endl << endl;
-	
-	sscflp.setDonnees(&d20x10_4);
+	SSCFLP sscflp(&d);
 	sscflp.glpkModeliserProbleme();
 	debut = clock();
 	sscflp.glpkResoudreProbleme();
@@ -54,21 +23,5 @@ int main(int argc, char* argv[]) {
 	sscflp.glpkAfficherSolution();
 	cout << "Résolution en " << (fin-debut)*1000/CLOCKS_PER_SEC << " millisecondes" << endl << endl;
 		
-	sscflp.setDonnees(&d30x15_2);
-	sscflp.glpkModeliserProbleme();
-	debut = clock();
-	sscflp.glpkResoudreProbleme();
-	fin = clock();
-	sscflp.glpkAfficherSolution();
-	cout << "Résolution en " << (fin-debut)*1000/CLOCKS_PER_SEC << " millisecondes" << endl << endl;
-	
-	sscflp.setDonnees(&d30x15_1);
-	sscflp.glpkModeliserProbleme();
-	debut = clock();
-	sscflp.glpkResoudreProbleme();
-	fin = clock();
-	cout << "Résolution en " << (fin-debut)*1000/CLOCKS_PER_SEC << " millisecondes" << endl << endl;
-
-	
 	return 0;
 }
